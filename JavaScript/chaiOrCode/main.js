27758bb137087964669c8b5c4cd2c88ebbca22af
Sahil Saini <76941329+sahilsaini1107@users.noreply.github.com>
2023-11-06 18:49:54 +0000

    js chai

---
+++ JavaScript/chaiOrCode/main.js
@@ -0,0 +1,28 @@
+/* *********** Variables and Datatypes Start ********* */
+// initially javascript scope ka use hi nhi krti thi. 
+// var -> let and const
+"use strict"; // treat all js code as newer version
+// null is standalone value -> representation of empty value.
+//undefined means abhi value assign nhi hui h.
+//symbol => unique ki baat hogi to ye aayega
+//null ka type hota h object
+// undefined ka type hot h undefined
+// NaN ka type hota h number
+
+/* *********** Variables and Datatypes End ********* */
+
+
+/* *********** Conversions and Operations Start ********* */
+let score = "33abc"
+score = null;
+let value = Number(score);
+console.log(value);//0
+//"33" -> 33
+//"asad" -> NaN
+//true -> 1
+
+//boolean
+//""->flase
+//"sdf"->true
+//1->true 0->false
+
