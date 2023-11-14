7a3e97143c38746cb10e9b827cecb4ed67599d02
Sahil Saini <76941329+sahilsaini1107@users.noreply.github.com>
2023-11-14 04:26:02 +0000

    hello maths

---
+++ JavaScript/chaiOrCode/datatype.js
@@ -0,0 +1,14 @@
+// data to memory m kis trha se rakha jaata h uss basis p data ko 2 categories m divide kiya h premitive and non premitive.
+
+let a = "heLlo";
+let b = new String("hi");
+// console.log(a.__proto__);
+// console.log(b.__proto__);
+
+// ************************************** string **************************************
+// few of methods which are available are ... 
+console.log(a.charAt(3));
+console.log(a.toUpperCase()); 
+console.log(a.toLowerCase()); 
+console.log(a.indexOf('o'));
+console.log(a.length);
